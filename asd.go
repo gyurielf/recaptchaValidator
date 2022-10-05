5f3ee38d3d8f79fe927aad76105defd4ae131f6b
gyurielf <mail.gyorgy.kallai@gmail.com>
2022-10-05 18:53:14 +0200

    initial commit

---
+++ asd.go
@@ -0,0 +1,60 @@
+package recaptcha
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/volatiletech/null"
+)
+
+const endpoint = "https://www.google.com/recaptcha/api/siteverify"
+
+type Response struct {
+	Success bool         `json:"success"`
+	Score   null.Float64 `json:"score,omitempty"`
+}
+
+type Error struct {
+	Message string `json:"message"`
+}
+
+func Verify(w http.ResponseWriter, r *http.Request) {
+	// ctx := r.Context()
+	req, err := http.NewRequest(http.MethodPost, endpoint, nil)
+	if err != nil {
+		errorResponse(w, err, "invalid request body")
+		log.Print(err)
+		return
+	}
+
+	q := r.URL.Query()
+	q.Add("secret", "123123123123key123123123")
+	req.URL.RawQuery = q.Encode()
+
+	// rawResp, err := hyper.Client().Do(req)
+	rawResp, err := http.Get(endpoint)
+
+	if err != nil {
+		log.Print(err.Error())
+		return
+	}
+	defer rawResp.Body.Close()
+
+	var respBody Response
+	if err = json.NewDecoder(rawResp.Body).Decode(&respBody); err != nil {
+		errorResponse(w, err, "invalid request body")
+		log.Print("error decoding recaptcha body")
+		return
+	}
+
+	// hyper.Success(ctx, w, respBody)
+}
+
+func errorResponse(w http.ResponseWriter, err error, message string) {
+	w.WriteHeader(http.StatusBadRequest)
+	raw, _ := json.Marshal(Error{
+		Message: message,
+	})
+	w.Write(raw)
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/gyurielf/recaptchaValidator
+
+go 1.19
+
+require github.com/volatiletech/null v8.0.0+incompatible
+
+require (
+	github.com/friendsofgo/errors v0.9.2 // indirect
+	github.com/gofrs/uuid v4.3.0+incompatible // indirect
+	github.com/volatiletech/inflect v0.0.1 // indirect
+	github.com/volatiletech/sqlboiler v3.7.1+incompatible // indirect
+	golang.org/x/xerrors v0.0.0-20190717185122-a985d3407aa7 // indirect
+)
